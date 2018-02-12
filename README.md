1d7b650d18a0f280e0028728602b5855b4a0b057
Dominic Tarr <dominic.tarr@gmail.com>
2018-02-12 13:37:04 +1300

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# patchavatar-recipients
+
+adds recipient avatars via `message.meta`
+to show recipients to a private message/thread
+
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,38 @@
+var isFeed = require('ssb-ref').isFeed
+var h = require('hyperscript')
+
+exports.gives = {
+  message: {
+    meta: true
+  }
+}
+
+exports.needs = {
+  identity: { main: 'first' },
+  avatar: {
+    name: 'first', image: 'first'
+  }
+}
+
+exports.create = function (api) {
+  return { message: { meta: function (data) {
+    if(!data.value.content.recps) return
+    return h('div.Avatar', data.value.content.recps.map(function (e) {
+      e = isFeed(e) ? e : isFeed(e.link) ? e.link : null
+      if(e != api.identity.main() && e != data.value.author)
+        //hmm: maybe link to private threads by this id?
+        return api.avatar.image(e)
+    }).filter(Boolean))
+
+  }}}
+}
+
+
+
+
+
+
+
+
+
+
